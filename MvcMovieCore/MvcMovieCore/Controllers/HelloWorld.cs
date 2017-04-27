0b3153e65e0ca279c8ba05c313fc52be1f2078ab
cmunhozprog02Hotmail <cmunhozprog02@hotmail.com>
2017-04-27 16:59:00 -0300

    Second

---
+++ MvcMovieCore/MvcMovieCore/Controllers/HelloWorld.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcMovieCore.Controllers
+{
+    public class HelloWorld : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View("Esta é minha ação padrão");
+        }
+        public IActionResult Welcome()
+        {
+            return View("Seja bem vindo, metodo da ação");
+        }
+    }
+}
